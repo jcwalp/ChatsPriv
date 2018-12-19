9b439e1dbf31f29738ddb51c45e6e478914b99d2
jon <43426006+jcwalp@users.noreply.github.com>
2018-12-19 13:52:00 -0500

    Update

--- username.js
+++ username.js
@@ -1,11 +1,1 @@
-function setName(){
-  const name = document.getElementById('username');
-}
 
-function buildList(){
-  console.log(name.toString());
-}
-
-function printName(){
-  console.log(name.toString());
-}
